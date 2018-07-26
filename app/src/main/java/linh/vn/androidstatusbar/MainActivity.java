72df4ed20c3fbb9882592980e26241f2a460e05f
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2018-07-26 14:52:32 +0700

    first commit

---
+++ app/src/main/java/linh/vn/androidstatusbar/MainActivity.java
@@ -0,0 +1,50 @@
+package linh.vn.androidstatusbar;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_set_light).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLightStatusBar(MainActivity.this);
+            }
+        });
+
+        findViewById(R.id.button_clear_light).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clearLightStatusBar(MainActivity.this);
+            }
+        });
+    }
+
+    private void setLightStatusBar(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int flags = activity.getWindow().getDecorView().getSystemUiVisibility();
+            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            activity.getWindow().getDecorView().setSystemUiVisibility(flags);
+            activity.getWindow().setStatusBarColor(Color.GRAY); // optional
+        }
+    }
+
+    private void clearLightStatusBar(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int flags = activity.getWindow().getDecorView().getSystemUiVisibility();
+            flags = flags ^ View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            activity.getWindow().getDecorView().setSystemUiVisibility(flags);
+            activity.getWindow().setStatusBarColor(Color.GREEN); // optional
+        }
+    }
+}
